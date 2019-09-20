f991114c9065706bb02aa01f26a0c48262fb8ff6
sqoor <alsqoorAbdullah@gmail.com>
2019-09-20 10:24:50 +0300

    reformate render students with new schema and  delete and updated options

--- ui/src/components/admin/Row.js
+++ ui/src/components/admin/Row.js
@@ -1,39 +1,62 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Form from "./Form";
 
 export class Row extends Component {
-    render() {
-        const {number, student, comp} = this.props; 
-        return (
-            <tr>
-                <th scope="row">{number}</th>
-                <td>{student.name}</td>
-                <td>{comp[0] ? comp[0].level.one : null}</td>
-                <td>{comp[0] ? comp[0].level.two : null}</td>
-                <td>{comp[0] ? comp[0].level.three : null}</td>
-                <td>{comp[1] ? comp[1].level.one : null}</td>
-                <td>{comp[1] ? comp[1].level.two : null}</td>
-                <td>{comp[1] ? comp[1].level.three : null}</td>
-                <td>{comp[2] ? comp[2].level.one : null}</td>
-                <td>{comp[2] ? comp[2].level.two : null}</td>
-                <td>{comp[2] ? comp[2].level.three : null}</td>
-                <td>{comp[3] ? comp[3].level.one : null}</td>
-                <td>{comp[3] ? comp[3].level.two : null}</td>
-                <td>{comp[3] ? comp[3].level.three : null}</td>
-                <td>{comp[4] ? comp[4].level.one : null}</td>
-                <td>{comp[4] ? comp[4].level.two : null}</td>
-                <td>{comp[4] ? comp[4].level.three : null}</td>
-                <td>{comp[5] ? comp[5].level.one : null}</td>
-                <td>{comp[5] ? comp[5].level.two : null}</td>
-                <td>{comp[5] ? comp[5].level.three : null}</td>
-                <td>{comp[6] ? comp[6].level.one : null}</td>
-                <td>{comp[6] ? comp[6].level.two : null}</td>
-                <td>{comp[6] ? comp[6].level.three : null}</td>
-                <td>{comp[7] ? comp[7].level.one : null}</td>
-                <td>{comp[7] ? comp[7].level.two : null}</td>
-                <td>{comp[7] ? comp[7].level.three : null}</td>
-            </tr>
-        )
-    }
+  updateStudent = updatedStudent => {
+    const studentId = this.props.student._id;
+    this.props.updateStudent(studentId, updatedStudent);
+  };
+
+  render() {
+    const { number, student, deleteStudent, editMode } = this.props;
+
+    return (
+      <tr>
+        <th style={{ display: editMode ? "none" : "table-cell" }} scope="row">
+          <button
+            onClick={deleteStudent.bind(this, student._id)}
+            className="btn btn-danger"
+          >
+            Delete
+          </button>
+        </th>
+        <th style={{ display: editMode ? "none" : "table-cell" }} scope="row">
+          <Form
+            addStudent={this.updateStudent}
+            title="Update"
+            defaultStudent={student}
+            styleClasses="btn btn-warning"
+          />
+        </th>
+        <th scope="row">{number}</th>
+        <td>{student.name}</td>
+        <td>{student.comp1 ? student.comp1.imitate : null}</td>
+        <td>{student.comp1 ? student.comp1.adapt : null}</td>
+        <td>{student.comp1 ? student.comp1.implement : null}</td>
+        <td>{student.comp2 ? student.comp2.imitate : null}</td>
+        <td>{student.comp2 ? student.comp2.adapt : null}</td>
+        <td>{student.comp2 ? student.comp2.implement : null}</td>
+        <td>{student.comp3 ? student.comp3.imitate : null}</td>
+        <td>{student.comp3 ? student.comp3.adapt : null}</td>
+        <td>{student.comp3 ? student.comp3.implement : null}</td>
+        <td>{student.comp4 ? student.comp4.imitate : null}</td>
+        <td>{student.comp4 ? student.comp4.adapt : null}</td>
+        <td>{student.comp4 ? student.comp4.implement : null}</td>
+        <td>{student.comp5 ? student.comp5.imitate : null}</td>
+        <td>{student.comp5 ? student.comp5.adapt : null}</td>
+        <td>{student.comp5 ? student.comp5.implement : null}</td>
+        <td>{student.comp6 ? student.comp6.imitate : null}</td>
+        <td>{student.comp6 ? student.comp6.adapt : null}</td>
+        <td>{student.comp6 ? student.comp6.implement : null}</td>
+        <td>{student.comp7 ? student.comp7.imitate : null}</td>
+        <td>{student.comp7 ? student.comp7.adapt : null}</td>
+        <td>{student.comp7 ? student.comp7.implement : null}</td>
+        <td>{student.comp8 ? student.comp8.imitate : null}</td>
+        <td>{student.comp8 ? student.comp8.adapt : null}</td>
+        <td>{student.comp8 ? student.comp8.implement : null}</td>
+      </tr>
+    );
+  }
 }
 
-export default Row
+export default Row;
